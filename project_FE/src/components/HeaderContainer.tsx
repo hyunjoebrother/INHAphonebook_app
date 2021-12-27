1f878ab582373d3f0c3b8cbf6a495b662c95eb78
hyunjoebrother <hyunjoe31@gmail.com>
2021-12-27 16:13:41 +0900

    211227_implement Header Search

--- project_FE/src/components/HeaderContainer.tsx
+++ project_FE/src/components/HeaderContainer.tsx
@@ -6,6 +6,7 @@ const HeaderContainer: React.FC = () => {
       <div>
         <strong>인하대학교 전화번호부</strong>
         <div className="search-container">
+          <img src="..\assets\icon\Search.svg" alt="" />
           <input type="text" placeholder="연락처가 궁금한 곳을 입력하세요!" />
         </div>
       </div>
